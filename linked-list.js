7815c1cc7e824cf98472e6ab07020ca37a14fc88
Ralph Effting <ralfting@gmail.com>
2019-11-18 07:13:05 -0300

    Create method addHead for LinkedList

--- linked-list.js
+++ linked-list.js
@@ -3,9 +3,16 @@ function LinkedList() {
   this.tail = null;
 }
 
-function Node(value, next, prev) {
+function Node(value, prev, next) {
   this.value = value;
   this.next = next;
   this.prev = prev;
+}
+
+LinkedList.prototype.addHead = function addHead(value) {
+  const newNode = new Node(value, this.head, null);
+  if(this.head) this.head.prev = newNode;
+  else this.tail = newNode;
+  this.head = newNode;
 }
 
