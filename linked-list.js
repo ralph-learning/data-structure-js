de0fd5f7a4df6aa1d2fb24ca8a149650f46666c4
Ralph Effting <ralfting@gmail.com>
2019-11-19 08:00:54 -0300

    Implement removeTail and removeHead

--- linked-list.js
+++ linked-list.js
@@ -9,18 +9,36 @@ function Node(value, next, prev) {
   this.prev = prev;
 }
 
-LinkedList.prototype.addHead = function addHead(value) {
+LinkedList.prototype.addHead = function(value) {
   const newNode = new Node(value, this.head, null);
   if(this.head) this.head.prev = newNode;
   else this.tail = newNode;
   this.head = newNode;
 }
 
-LinkedList.prototype.addTail = function addTail(value) {
+LinkedList.prototype.addTail = function(value) {
   const newNode = new Node(value, null, this.tail);
   if(this.tail) this.tail.next = newNode;
   else this.head = newNode;
   this.tail = newNode;
+}
+
+LinkedList.prototype.removeHead = function() {
+  if(!this.head) return null;
+  const value = this.head.value;
+  this.head = this.head.next;
+  if(this.head) this.head.prev = null;
+  else this.tail = null;
+  return value;
+}
+
+LinkedList.prototype.removeTail = function() {
+  if(!this.tail) return null;
+  const value = this.tail.value;
+  this.tail = this.tail.prev;
+  if(this.tail) this.tail.next = null;
+  else this.head = null;
+  return value;
 }
 
 const ll = new LinkedList();
@@ -31,5 +49,6 @@ ll.addHead(300);
 
 ll.addTail(50);
 
-console.log(ll)
+ll.removeTail();
+console.log(ll.removeTail())
 
