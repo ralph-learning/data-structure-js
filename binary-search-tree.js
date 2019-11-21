393d07d177205638b54a61111e87125c4497b29f
Ralph Effting <ralfting@gmail.com>
2019-11-21 08:40:47 -0300

    Create depthFirstTraversal in order method

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -6,8 +6,8 @@ function BST(value) {
 
 BST.prototype.insert = function(value) {
   if(value <= this.value) {
-    if(!this.left) this.left = new BST(value);// is null
-    else this.left.insert(value); // is there already a node in there
+    if(!this.left) this.left = new BST(value);
+    else this.left.insert(value);
   } else if(value > this.value) {
     if(!this.right) this.right = new BST(value);
     else this.right.insert(value);
@@ -26,6 +26,12 @@ BST.prototype.contains = function(value) {
   }
 };
 
+BST.prototype.depthFirstTransversal = function(itetatorFunc) {
+  if(this.left) this.left.depthFirstTransversal(itetatorFunc);
+  itetatorFunc(this.value);
+  if(this.right) this.right.depthFirstTransversal(itetatorFunc);
+}
+
 const bst = new BST(100);
 
 bst.insert(10);
@@ -33,7 +39,9 @@ bst.insert(101);
 bst.insert(5);
 bst.insert(7);
 
-console.log(bst.contains(50)); // false
-console.log(bst.contains(7)); // true
+// console.log(bst.contains(50)); // false
+// console.log(bst.contains(7)); // true
+
+bst.depthFirstTransversal(function(value) { console.log(value); })
 
 
