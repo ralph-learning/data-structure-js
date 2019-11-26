4dd22a13497df603739fe4974dfe43c64041609b
Ralph Effting <ralfting@gmail.com>
2019-11-26 08:51:36 -0300

    Add suport to change value

--- hash-table.js
+++ hash-table.js
@@ -22,16 +22,28 @@ HashTable.prototype.hash = function(key) {
 HashTable.prototype.insert = function(key, value) {
   const index = this.hash(key);
   if(!this.bucket[index]) this.bucket[index] = new HashNode(key, value);
+  else if(this.bucket[index].key === key) {
+    this.bucket[index].value = value;
+  }
   else {
     let currentNode = this.bucket[index];
     while(currentNode.next) {
       currentNode = currentNode.next;
+      if(currentNode.next.key === key) {
+        currentNode.next.value = value;
+        return;
+      }
     }
     currentNode.next = new HashNode(key, value);
   }
 }
 
 const myHT = new HashTable(30);
-myHT.insert('name', 'Ralph');
-myHT.insert('test', 'Marina');
+
+myHT.insert('Joao', 'joao@example.co');
+myHT.insert('Joao', 'joaoupdate@gmail.com');
+myHT.insert('Gabi', 'gabi@gmail.com')
+myHT.insert('Gabi', 'gabi@yahoo.com')
+myHT.insert('Giba', 'giba@gmail.com')
+
 console.log(myHT);
